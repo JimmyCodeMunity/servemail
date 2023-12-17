cc12d90563b7a0f4417875c9630642bd23b1c4de
Jimmy <96749667+JimmyCodeMunity@users.noreply.github.com>
2023-12-17 13:57:47 +0300

    Update Congrats.jsx

--- src/pages/Congrats.jsx
+++ src/pages/Congrats.jsx
@@ -21,7 +21,7 @@ function Congrats() {
         }
         try {
             setIsLoading(true)
-            const response = await axios.post('https://anonimous-mailserver.vercel.app/sendcongrats',{email:receiverEmail});
+            const response = await axios.post('https://persmailer.vercel.app/sendcongrats',{email:receiverEmail});
             //toast.success(`Save ${response.data.receiver} successfully`);
             setIsLoading(false);
             Swal.fire({
