1011f3101cd5f179c16de281b0d249f32ec2a5cd
Jimmy <96749667+JimmyCodeMunity@users.noreply.github.com>
2023-12-17 13:58:37 +0300

    Update PassMissmatch.jsx

--- src/pages/PassMissmatch.jsx
+++ src/pages/PassMissmatch.jsx
@@ -21,7 +21,7 @@ function PassMissmatch() {
         }
         try {
             setIsLoading(true)
-            const response = await axios.post('https://anonimous-mailserver.vercel.app/sendpassmissmatch',{email:receiverEmail});
+            const response = await axios.post('https://persmailer.vercel.app/sendpassmissmatch',{email:receiverEmail});
             //toast.success(`Save ${response.data.receiver} successfully`);
             setIsLoading(false);
             Swal.fire({
